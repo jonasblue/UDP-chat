6492ce5bbce619ee0b17fac113484b9fc23b21a1
JB <jb.durville@telecom-bretagne.eu>
2017-11-29 16:08:42 +0100

    operational thru shell

--- listen.sh
+++ listen.sh
@@ -15,15 +15,13 @@ help_function()
 	local bold=$(tput bold)
 	local normal=$(tput sgr0)
 	echo "${bold}NAME"
-    echo "		${normal}connect.sh - connects host to a remote machine using <netcat -u> (UDP)"
+    echo "		${normal}listen.sh - listens to a UDP connection from a remote machine"
 	echo "${bold}SYNOPSIS"
-	echo "		${normal}connect.sh [OPTIONS] -a ip_address -p port"
+	echo "		${normal}listen.sh [OPTIONS] -p port"
 	echo "${bold}DESCRIPTION"
-	echo "		${normal}This script takes as argument an IP address and a port and connects to them using netcat and the UDP protocol."
+	echo "		${normal}This script takes as argument a port and listens for any UDP connection on that port."
 	echo "		${bold}-h, --help"
 	echo "			${normal}displays this help menu"
-	echo "		${bold}-a, --address"
-	echo "			${normal}IP address of the remote machine the user wants to connect to."
 	echo "		${bold}-p, --port"
 	echo "			${normal}port of the remote machine used for the UDP connection. The port needs to be open and listening for incoming UDP connections."
 }
@@ -37,9 +35,6 @@ while [ "$1" != "" ]; do
 				help_function
 				exit
 				;;
-		-a | --address )shift
-				IP_ADDRESS=$1
-				;;
 		-p | --port )	shift
 				PORT=$1
 				;;
@@ -50,15 +45,11 @@ while [ "$1" != "" ]; do
 	shift
 done
 
-if [[ "$IP_ADDRESS" == "" ]]; then
-    echo -e "${GREEN}[INFO]${NC} no IP address so using localhost instead: 127.0.0.1"
-    IP_ADDRESS="127.0.0.1"
-fi
 if [[ "$PORT" == "" ]]; then
     echo -e "${RED}[ERROR]${NC} missing a port"
     exit
 fi
 
-echo "About to connect to $IP_ADDRESS:$PORT"
-nc -uvv $IP_ADDRESS $PORT
+nc -luvv -p $PORT
+echo "Listening for UDP incoming on $PORT"
 exit
